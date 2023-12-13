cd25288a43338cafdd864a86583c4cf532fe8e49
aidant <15520814+aidant@users.noreply.github.com>
2023-12-13 22:52:51 +1100

    add gamemode to map data

--- src/lib/interpreter-overwatch-player-map-stat.ts
+++ src/lib/interpreter-overwatch-player-map-stat.ts
@@ -24,25 +24,46 @@ const result_mappings = {
   'Offense Rpl {0}:Win:Wins;': 'win',
 }
 
+const gamemode_mappings = {
+  'Circuit Royal': 'Escort',
+  'Dorado': 'Escort',
+  'Route 66': 'Escort',
+  'Junkertown': 'Escort',
+  'Rialto': 'Escort',
+  'Havana': 'Escort',
+  'Watchpoint: Gibraltar': 'Escort',
+  'Shambali Monastery': 'Escort',
+  'Blizzard World': 'Hybrid',
+  'Numbani': 'Hybrid',
+  'Hollywood': 'Hybrid',
+  'Eichenwalde': 'Hybrid',
+  "King's Row": 'Hybrid',
+  'Midtown': 'Hybrid',
+  'Paraíso': 'Hybrid',
+  'Busan': 'Control',
+  'Nepal': 'Control',
+  'Ilios': 'Control',
+  'Oasis': 'Control',
+  'Lijiang Tower': 'Control',
+  'Antarctic Peninsula': 'Control',
+  'Samoa': 'Control',
+  'Colosseo': 'Push',
+  'Esperança': 'Push',
+  'New Queen Street': 'Push',
+  'New Junk City': 'Flashpoint',
+  'Suravasa': 'Flashpoint',
+  'Hanamura': 'Assault',
+  'Horizon Lunar Colony': 'Assault',
+  'Paris': 'Assault',
+  'Temple of Anubis': 'Assault',
+  'Volskaya Industries': 'Assault',
+}
+
 export interface Filter {
   season?: string
 }
 
-export const interpreterOverwatchPlayerMapStat = (data: BlizzardData, filter: Filter) => {
-  const maps = data['overwatch_pc']['player_map_stat'].reduce((maps: any, map: any) => {
-    if (filter.season && map.stat_context_name !== filter.season) {
-      return maps
-    }
-
-    const name = map_mappings[map.map as keyof typeof map_mappings] || map.map
-
-    maps[name] ??= {}
-    maps[name][result_mappings[map.stat as keyof typeof result_mappings]] ??= 0
-    maps[name][result_mappings[map.stat as keyof typeof result_mappings]] += map.amount
-
-    return maps
-  }, {})
-
+const showMapStats = (maps: Record<string, { win: number; enter: number; tie: number }>) => {
   console.log(
     JSON.stringify(
       Object.fromEntries(
@@ -69,4 +90,39 @@ export const interpreterOverwatchPlayerMapStat = (data: BlizzardData, filter: Fi
       2,
     ),
   )
+}
+
+export const interpreterOverwatchPlayerMapStat = (data: BlizzardData, filter: Filter) => {
+  const maps: Record<string, { win: number; enter: number; tie: number }> = data['overwatch_pc'][
+    'player_map_stat'
+  ].reduce((maps: any, map: any) => {
+    if (filter.season && map.stat_context_name !== filter.season) {
+      return maps
+    }
+
+    const name = map_mappings[map.map as keyof typeof map_mappings] || map.map
+
+    maps[name] ??= {}
+    maps[name][result_mappings[map.stat as keyof typeof result_mappings]] ??= 0
+    maps[name][result_mappings[map.stat as keyof typeof result_mappings]] += map.amount
+
+    return maps
+  }, {})
+
+  showMapStats(maps)
+
+  const gamemodes: Record<string, { win: number; enter: number; tie: number }> = {}
+
+  for (const [name, map] of Object.entries(maps)) {
+    gamemodes[gamemode_mappings[name as keyof typeof gamemode_mappings]] ??= {
+      win: 0,
+      enter: 0,
+      tie: 0,
+    }
+    gamemodes[gamemode_mappings[name as keyof typeof gamemode_mappings]].enter += map.enter
+    gamemodes[gamemode_mappings[name as keyof typeof gamemode_mappings]].win += map.win
+    gamemodes[gamemode_mappings[name as keyof typeof gamemode_mappings]].tie += map.tie
+  }
+
+  showMapStats(gamemodes)
 }
